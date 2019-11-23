d297cb06fa0c2db4f622797dda4585c4f0cdd0fe
chc1129 <chc1129@gmail.com>
2019-11-24 08:41:38 +0900

    Shootimg commit

---
+++ Shooting/app/src/main/java/com/example/shooting/GameView.java
@@ -0,0 +1,32 @@
+package com.example.shooting;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class GameView extends View {
+
+    private Droid droid;
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        if (droid == null) {
+            Bitmap droidBitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.droid);
+            droid = new Droid(droidBitmap, width, height);
+        }
+
+        droid.draw(canvas);
+    }
+}
